c8b311def1623fa8cc6fbdb94b3898b0d9362b40
Vishnu-Prasath3 <eeevishnuprasath@gmail.com>
2025-02-08 23:08:04 +0530

    vite config+signup

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-const target = "http://localhost:5000/api";
-
+const target = "https://fsd-capstone.onrender.com";
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target,
-				changeOrigin: true,
-				secure: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target,
+        changeOrigin: true,
+        secure: false,
+        
+      },
+    },
+  },
 });
