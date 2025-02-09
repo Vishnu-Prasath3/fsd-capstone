736f1d01660048b10d26e8d89116deefdcec618d
Vishnu-Prasath3 <eeevishnuprasath@gmail.com>
2025-02-09 12:27:07 +0530

    vite added && git ignore removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//       target: "https://fsd-capstone.onrender.com",
-//       changeOrigin: true,
-//       secure:false,
-//     },
-//   },
-// }
+  server: {
+    proxy: {
+      "/api": {
+      target: "https://fsd-capstone.onrender.com",
+      changeOrigin: true,
+      secure:false,
+    },
+  },
+}
 });
